350df703a322202e22794187226fb3378e9a10e7
Rasik Jain <jainrasik@gmail.com>
2018-11-26 13:41:49 +0530

    button events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { CourseService } from './course/course.service';
 
 @NgModule({
   declarations: [AppComponent, CourseComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [CourseService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from './course.service';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-
   title = 'list of courses';
-  courses = ['course1', 'course2', 'course3'];
+  imageUrl = 'http://lorempixel.com/g/400/200/';
+  courses;
+  isActive = true;
+
+  constructor(service: CourseService) {
+    this.courses = service.getCourses();
+  }
+
+  onSave($event: MouseEvent) {
+    console.log('button was clicked', $event);
+    $event.stopPropagation();
+  }
+  ngOnInit() {}
 }
